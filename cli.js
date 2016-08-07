3e9605b8a1de047c5ce12bb8d500a2e0903e4f21
Mark Lee <electronjs@lazymalevolence.com>
2016-08-07 16:57:51 -0700

    Convert to ES6

--- cli.js
+++ cli.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
-var download = require('./')
-var minimist = require('minimist')
-var opts = minimist(process.argv.slice(2))
+
+'use strict'
+
+const download = require('./')
+const minimist = require('minimist')
+
+let opts = minimist(process.argv.slice(2))
 
 if (opts['strict-ssl'] === false) {
   opts.strictSSL = false
 }
 
-download(opts, function (err, zipPath) {
+download(opts, (err, zipPath) => {
   if (err) throw err
   console.log('Downloaded zip:', zipPath)
   process.exit(0)
