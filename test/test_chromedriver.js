4750940bb39c4168d5726f7ae5dc02258e4404b6
Kevin Sawicki <kevinsawicki@gmail.com>
2017-05-10 12:59:57 -0700

    Support new version in Chromedriver asset name

--- test/test_chromedriver.js
+++ test/test_chromedriver.js
@@ -5,7 +5,7 @@ const path = require('path')
 const test = require('tape')
 const verifyDownloadedZip = require('./helpers').verifyDownloadedZip
 
-test('Chromedriver test', (t) => {
+test('Chromedriver test with Chromedriver version in asset name in Electron < 1.7.0', (t) => {
   download({
     version: '1.4.0',
     arch: 'x64',
@@ -15,6 +15,20 @@ test('Chromedriver test', (t) => {
   }, (err, zipPath) => {
     verifyDownloadedZip(t, err, zipPath)
     t.ok(/^chromedriver-v2\.21-/.test(path.basename(zipPath)), 'Zip path should start with chromedriver-v.2.21')
+    t.end()
+  })
+})
+
+test('Chromedriver test with Electron version in asset name in Electron 1.7.0+', (t) => {
+  download({
+    version: '1.7.0',
+    arch: 'x64',
+    platform: 'darwin',
+    chromedriver: true,
+    quiet: true
+  }, (err, zipPath) => {
+    verifyDownloadedZip(t, err, zipPath)
+    t.ok(/^chromedriver-v1\.7\.0-/.test(path.basename(zipPath)), 'Zip path should start with chromedriver-v1.7.0')
     t.end()
   })
 })
