02e4c05c3153f738ed482fa67ad5d5a57209ed3f
Mark Lee <electronjs@lazymalevolence.com>
2019-04-30 00:45:17 -0700

    chore: upgrade sumchecker to ^3.0.0

--- typings/ambient.d.ts
+++ typings/ambient.d.ts
@@ -1,13 +1,1 @@
-declare module 'sumchecker' {
-  type SumChecker = (
-    algo: string,
-    shasumFilePath: string,
-    baseDir: string,
-    files: string[],
-  ) => Promise<void>;
-
-  const fn: SumChecker;
-  export = fn;
-}
-
 declare module 'env-paths';
