ba23eed56d3659671dba2a401929abb1aefe2061
Mark Lee <electronjs@lazymalevolence.com>
2019-05-25 10:36:18 -0700

    feat: add request options for GotDownloader

--- src/GotDownloader.ts
+++ src/GotDownloader.ts
@@ -1,15 +1,16 @@
 import * as fs from 'fs-extra';
 import * as got from 'got';
+import { RequestOptions } from 'https';
 import * as path from 'path';
 
 import { Downloader } from './Downloader';
 
-export class GotDownloader implements Downloader<null> {
-  async download(url: string, targetFilePath: string) {
+export class GotDownloader implements Downloader<RequestOptions> {
+  async download(url: string, targetFilePath: string, options: RequestOptions) {
     await fs.mkdirp(path.dirname(targetFilePath));
     const writeStream = fs.createWriteStream(targetFilePath);
     await new Promise((resolve, reject) => {
-      const downloadStream = got.stream(url);
+      const downloadStream = got.stream(url, options);
       downloadStream.pipe(writeStream);
 
       downloadStream.on('error', error => reject(error));
