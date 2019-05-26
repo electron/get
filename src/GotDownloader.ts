44f44a3bbf00f49af51c7814994a3bf2fff40ffe
Mark Lee <electronjs@lazymalevolence.com>
2019-05-26 09:41:29 -0700

    got expects any according to the source code 🤷

--- src/GotDownloader.ts
+++ src/GotDownloader.ts
@@ -1,12 +1,11 @@
 import * as fs from 'fs-extra';
 import * as got from 'got';
-import { RequestOptions } from 'https';
 import * as path from 'path';
 
 import { Downloader } from './Downloader';
 
-export class GotDownloader implements Downloader<RequestOptions> {
-  async download(url: string, targetFilePath: string, options: RequestOptions) {
+export class GotDownloader implements Downloader<any> {
+  async download(url: string, targetFilePath: string, options?: any) {
     await fs.mkdirp(path.dirname(targetFilePath));
     const writeStream = fs.createWriteStream(targetFilePath);
     await new Promise((resolve, reject) => {
