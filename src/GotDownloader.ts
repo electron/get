0a64fd4b983dbb353b5f927334e22be785f0fa21
Mark Lee <malept@users.noreply.github.com>
2019-05-27 13:35:27 -0700

    feat: pass in downloader options to got (#90)
    * feat: add request options for GotDownloader
    
    * got expects any according to the source code 🤷

--- src/GotDownloader.ts
+++ src/GotDownloader.ts
@@ -4,12 +4,12 @@ import * as path from 'path';
 
 import { Downloader } from './Downloader';
 
-export class GotDownloader implements Downloader<null> {
-  async download(url: string, targetFilePath: string) {
+export class GotDownloader implements Downloader<any> {
+  async download(url: string, targetFilePath: string, options?: any) {
     await fs.mkdirp(path.dirname(targetFilePath));
     const writeStream = fs.createWriteStream(targetFilePath);
     await new Promise((resolve, reject) => {
-      const downloadStream = got.stream(url);
+      const downloadStream = got.stream(url, options);
       downloadStream.pipe(writeStream);
 
       downloadStream.on('error', error => reject(error));
