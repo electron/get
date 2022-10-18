4fe5b00548487a25e2fb1a8fc7665195524754cc
Black-Hole <158blackhole@gmail.com>
2022-10-19 01:52:18 +0800

    fix: remove global-tunnel-ng library (#227)

--- README.md
+++ README.md
@@ -134,9 +134,4 @@ disable, set the `ELECTRON_GET_NO_PROGRESS` environment variable to any non-empt
 ### Proxies
 
 Downstream packages should utilize the `initializeProxy` function to add HTTP(S) proxy support. If
-the environment variable `ELECTRON_GET_USE_PROXY` is set, it is called automatically. A different
-proxy module is used, depending on the version of Node in use, and as such, there are slightly
-different ways to set the proxy environment variables. For Node 10 and above,
-[`global-agent`](https://github.com/gajus/global-agent#environment-variables) is used. Otherwise,
-[`global-tunnel-ng`](https://github.com/np-maintain/global-tunnel#auto-config) is used. Refer to the
-appropriate linked module to determine how to configure proxy support.
+the environment variable `ELECTRON_GET_USE_PROXY` is set, it is called automatically.
--- src/proxy.ts
+++ src/proxy.ts
@@ -8,23 +8,18 @@ const d = debug('@electron/get:proxy');
  */
 export function initializeProxy(): void {
   try {
-    // Code originally from https://github.com/yeoman/yo/blob/b2eea87e/lib/cli.js#L19-L28
-    const MAJOR_NODEJS_VERSION = parseInt(process.version.slice(1).split('.')[0], 10);
+    // See: https://github.com/electron/get/pull/214#discussion_r798845713
+    const env = getEnv('GLOBAL_AGENT_');
 
-    if (MAJOR_NODEJS_VERSION >= 10) {
-      // See: https://github.com/electron/get/pull/214#discussion_r798845713
-      const env = getEnv('GLOBAL_AGENT_');
+    setEnv('GLOBAL_AGENT_HTTP_PROXY', env('HTTP_PROXY'));
+    setEnv('GLOBAL_AGENT_HTTPS_PROXY', env('HTTPS_PROXY'));
+    setEnv('GLOBAL_AGENT_NO_PROXY', env('NO_PROXY'));
 
-      setEnv('GLOBAL_AGENT_HTTP_PROXY', env('HTTP_PROXY'));
-      setEnv('GLOBAL_AGENT_HTTPS_PROXY', env('HTTPS_PROXY'));
-      setEnv('GLOBAL_AGENT_NO_PROXY', env('NO_PROXY'));
-
-      // `global-agent` works with Node.js v10 and above.
-      require('global-agent').bootstrap();
-    } else {
-      // `global-tunnel-ng` works with Node.js v10 and below.
-      require('global-tunnel-ng').initialize();
-    }
+    /**
+     * TODO: replace global-agent with a hpagent. @BlackHole1
+     * https://github.com/sindresorhus/got/blob/HEAD/documentation/tips.md#proxying
+     */
+    require('global-agent').bootstrap();
   } catch (e) {
     d('Could not load either proxy modules, built-in proxy support not available:', e);
   }
