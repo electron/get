13ede9ef5a7cd0cba3c43591bcbf10ad5766ddd2
Erick Zhao <erick@hotmail.ca>
2025-01-17 15:16:32 -0800

    A little more tweaking

--- src/downloader-resolver.ts
+++ src/downloader-resolver.ts
@@ -1,11 +1,12 @@
 import { DownloadOptions } from './types';
 import { Downloader } from './Downloader';
 
+// TODO: Resolve the downloader or default to GotDownloader
+// Current thoughts are a dot-file traversal for something like
+// ".electron.downloader" which would be a text file with the name of the
+// npm module to import() and use as the downloader
+import { GotDownloader } from './GotDownloader';
+
 export async function getDownloaderForSystem(): Promise<Downloader<DownloadOptions>> {
-  // TODO: Resolve the downloader or default to GotDownloader
-  // Current thoughts are a dot-file traversal for something like
-  // ".electron.downloader" which would be a text file with the name of the
-  // npm module to import() and use as the downloader
-  const { GotDownloader } = await import('./GotDownloader');
   return new GotDownloader();
 }
